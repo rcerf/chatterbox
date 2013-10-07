22d0a5f70f7dd7e6879efc03117c792a27cf8f3b
rcerf <rickcerf@gmail.com>
2013-10-07 16:30:01 -0700

    installed underscore, retrieved messages from Parse server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,9 @@
-// YOUR CODE HERE:
+$.get( "https://api.parse.com/1/classes/chatterbox", function( data ) {
+  $( ".timeline" ).html( data );
+  console.log(data);
+  // console.log(data.results);
+  var messages = data.results;
+  for(var i=12; i<messages.length; i++){
+    $(".timeline").append("<div>"+messages[i]["text"]+"</div>");
+  }
+});
