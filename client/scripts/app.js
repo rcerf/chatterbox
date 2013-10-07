c74c475366d66e1dbd41ed39e8aad38238db90b7
rcerf <rickcerf@gmail.com>
2013-10-07 16:44:32 -0700

    completed fetch and refresh

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,20 @@
-$.get( "https://api.parse.com/1/classes/chatterbox", function( data ) {
-  $( ".timeline" ).html( data );
-  console.log(data);
-  // console.log(data.results);
-  var messages = data.results;
-  for(var i=12; i<messages.length; i++){
-    $(".timeline").append("<div>"+messages[i]["text"]+"</div>");
-  }
-});
+var timelineLength = 12;
+
+var displayMessages = function(){
+  $.get( "https://api.parse.com/1/classes/chatterbox", function( data ) {
+    console.log("refreshed");
+    // console.log(data.results);
+    var messages = data.results;
+    for(var i = timelineLength; i < messages.length; i++){
+      var mes = messages[i];
+      $(".timeline").append("<div>"+
+        "<span class = 'username'>"+
+        mes.username +
+        ": </span>" +
+        "<span class = 'message'>"+ mes.text+ "</span>" + "</div>");
+    }
+    timelineLength = messages.length;
+  });
+};
+displayMessages();
+setInterval(displayMessages, 3000);
